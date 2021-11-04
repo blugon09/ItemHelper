31432d6472fe1829c28bb770a2dea36cf2d06331
Blugon <71944969+Blugon0921@users.noreply.github.com>
2021-11-04 17:59:32 +0900

    1.1.3-SNAPSHOT = TextComponent.rgb

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.github.blugon09"
-version = "1.1.2-SNAPSHOT"
+version = "1.1.3-SNAPSHOT"
 
 
 repositories {
--- src/main/kotlin/io/github/blugon09/itemhelper/ComponentHelper.kt
+++ src/main/kotlin/io/github/blugon09/itemhelper/ComponentHelper.kt
@@ -2,28 +2,8 @@ package io.github.blugon09.itemhelper
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.TextComponent
-import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.format.TextDecoration
-
-class ColorText {
-    val DARK_RED: NamedTextColor = NamedTextColor.DARK_RED
-    val RED: NamedTextColor = NamedTextColor.RED
-    val GOLD: NamedTextColor = NamedTextColor.GOLD
-    val YELLOW: NamedTextColor = NamedTextColor.YELLOW
-    val GREEN: NamedTextColor = NamedTextColor.GREEN
-    val LIGHT_GREEN: NamedTextColor = NamedTextColor.DARK_GREEN
-    val DARK_AQUA: NamedTextColor = NamedTextColor.DARK_AQUA
-    val AQUA: NamedTextColor = NamedTextColor.AQUA
-    val BLUE: NamedTextColor = NamedTextColor.BLUE
-    val DARK_BLUE: NamedTextColor = NamedTextColor.DARK_BLUE
-    val PINK: NamedTextColor = NamedTextColor.LIGHT_PURPLE
-    val PURPLE: NamedTextColor = NamedTextColor.DARK_PURPLE
-    val GRAY: NamedTextColor = NamedTextColor.GRAY
-    val DARK_GRAY: NamedTextColor = NamedTextColor.DARK_GRAY
-    val WHITE: NamedTextColor = NamedTextColor.WHITE
-    val BLACK: NamedTextColor = NamedTextColor.BLACK
-}
 
 
 fun String.component(): TextComponent {
